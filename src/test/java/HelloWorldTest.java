fcd8af7b2ec7a3a43548ec7dc11c3f82a914d583
V.Kovach <v.cova4@gmail.com>
2022-07-11 00:07:42 +0300

    commit 5

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -10,7 +10,8 @@ public class HelloWorldTest {
          * new commit1
          * new commit 2
          * new commit 3
-         *
+         * new commit 4
+         * new commit 5
          */
     }
 }
