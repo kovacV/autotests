bc4ae0e84ef5115eae1376901ffb5c2d6daedbcb
V.Kovach <v.cova4@gmail.com>
2022-07-10 19:58:22 +0300

    newB

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,6 +1,12 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class HelloWorldTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void test1() {
         System.out.println("Hello world");
+        System.out.println("newB");
+        Assert.assertEquals(1, 1);
     }
 }
