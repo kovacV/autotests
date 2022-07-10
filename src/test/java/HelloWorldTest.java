da1524d46057b53ab49d18a0b9bb42313188709f
V.Kovach <v.cova4@gmail.com>
2022-07-11 01:27:51 +0300

    statsuiasd

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -19,6 +19,6 @@ public class HelloWorldTest {
          */
         System.out.println("qwe");
         System.out.println("ewqrt");
-        System.out.println("test 3");
+        System.out.println("test 6");
     }
 }
