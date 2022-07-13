1342e22acd2a328a20aac9acdad1dad8677de51a
kovacV <107554144+kovacV@users.noreply.github.com>
2022-07-13 19:12:18 +0300

    ertetert

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -3,5 +3,6 @@ public class HelloWorldTest {
     public static void main(String[] args) {
         System.out.println("Hello world");
         //changes for pollSCM
+        //ertetertertet
     }
 }
